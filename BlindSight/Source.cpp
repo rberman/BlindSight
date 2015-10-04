c9cb603b1e925ec862a0cdb0f04d030df84a5905
MarcioPorto <mflporto@gmail.com>
2015-10-04 15:58:27 +0800

    setup 2

--- BlindSight/Source.cpp
+++ BlindSight/Source.cpp
@@ -1,8 +1,8 @@
-#include <vector>
+#include <windows.h>
+#include <wchar.h>
 #include "pxcsensemanager.h"
-#include "pxcsession.h"
 
-int main() {
+int wmain(int argc, WCHAR* argv[]) {
 	// create the PXCSenseManager
 	PXCSenseManager *psm = 0;
 	psm = PXCSenseManager::CreateInstance();
@@ -10,11 +10,30 @@ int main() {
 		wprintf_s(L"Unable to create the PXCSenseManager\n");
 		return 1;
 	}
-
+	// Retrieve the underlying session created by the PXCSenseManager.
+	// The returned instance is an PXCSenseManager internally managed object.
+	// Note: Do not release the session!
 	PXCSession *session;
 	session = psm->QuerySession();
-
+	if (session == NULL) {
+		wprintf_s(L"Session not created by PXCSenseManager\n");
+		return 2;
+	}
+	// query the session version
+	PXCSession::ImplVersion ver;
+	ver = session->QueryVersion();
+	// print version to console
+	wprintf_s(L" Hello Intel RSSDK Version %d.%d \n", ver.major, ver.minor);
+	// enumerate all available modules that are automatically loaded with the RSSDK
+	for (int i = 0;; i++) {
+		PXCSession::ImplDesc desc;
+		if (session->QueryImpl(0, i, &desc) < PXC_STATUS_NO_ERROR) break;
+		// Print the module friendly name and iuid (interface unique ID)
+		wprintf_s(L"Module[%d]: %s\n", i, desc.friendlyName);
+		wprintf_s(L" iuid=%x\n", desc.iuid);
+	}
+	// close the streams (if any) and release any session and processing module instances
+	psm->Release();
+	system(“pause”);
 	return 0;
 }
-
-
